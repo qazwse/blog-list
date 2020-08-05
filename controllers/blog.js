36df220d222e474cd4828af51288340122fc09f5
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-08-05 17:28:04 -0400

    Move models and controllers into their own files

---
+++ controllers/blog.js
@@ -0,0 +1,23 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+const { response } = require('express')
+
+blogRouter.get('/', (_, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+blogRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+module.exports = blogRouter
--- main.js
+++ main.js
@@ -3,17 +3,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogRouter = require('./controllers/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,23 +14,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (_, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
